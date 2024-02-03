2bbb722d742fbc28ae3e60e134358310ba2f9b5c
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2024-02-03 10:23:11 -0400

    Estructura de la arquitectura.

---
+++ Shopping/shopping.IntregrationTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace shopping.IntregrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Shopping/shopping.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace shopping.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
